ccd4f0bf29165dfa92ffe568b1515d049b822f93
Abdikani98 <abdikanishaarib>
2024-11-30 13:46:19 -0800

    Aas Aaska Javascript

---
+++ as.js
@@ -0,0 +1,26 @@
+let name = prompt("Enter Your Name ")
+let number = prompt("Enter Your Phone Number")
+let address = prompt("Enter Your Address")
+let birth_day = parseInt(prompt("Enter Your Birth day"))
+
+
+let age=2024-birth_day
+
+
+
+console.log("Name:", name)
+console.log("Number:", number)
+console.log("Address:", address)
+console.log("Age:", age)
+
+
+document.write("<h1> Name: ",name,"</h1>")
+document.write("<h2> Number: ", number,"</h2>")
+document.write("<h3> address: ", address , "</h3>")
+document.write("<h4> age: ", age , "</h4>")
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,48 @@
+/*let age = 30;
+
+document.write(age)
+
+console.log(age)
+*/
+
+// let age = 50;
+
+// let userName ="Abdikani";
+
+// let today = true
+
+// let num1 = 213.54
+
+// console.log(age, userName, today, num1)
+
+let additon = 30+20
+
+console.log("Additon:", 30+20 )
+console.log("Mub:", 30-20 )
+console.log("Mult:", 30*20 )
+console.log("Division:", 30/20 )
+console.log("Exp:", 30**20 )
+console.log("Remander:", 30%20 )
+
+let num3 = 40;
+num3 +=5
+console.log("Increment:", num3)
+
+let num4 =50;
+num4 -=8
+
+console.log("Decrement:", num4)
+
+
+
+// let num1 =30
+// let num2 =20
+
+// alert(num1 + num2)
+
+
+let num1 = parseInt(prompt("Enter num1"))
+let num2 = parseInt(prompt("Enter num2"))
+
+
+console.log(num1 + num2)
